9bc900a9ba1766fa39b4d1cba92f98c6c1e14cc4
Aneesh Kumar Mourya <aneeshkrmourya@gmail.com>
2026-09-08 17:22:12 +0530

    Enhance intro.py with NumPy array examples
    Added examples of creating and manipulating arrays using NumPy, including zeros, ones, empty arrays, and arange.

--- intro.py
+++ intro.py
@@ -57,3 +57,38 @@ print(a3.ndim)
 an = np.array([1,2,3,4], ndmin=10) # we can use ndim to tell how many dimenssion array should be 
 # print(an)
 # print(an.ndim)
+
+
+#Array filled with O'S"
+
+a = np.zeros(4, int) # 1D
+# print(a)
+# print()
+a1 = np.zeros((3,4), int)
+# print(a1)
+
+#array filled with 1's"
+a2 = np.ones(4, int) # 1D
+# print(a2)
+# print()
+a3 = np.ones((3,4), int)
+# print(a3)
+
+#empty array
+a4 = np.empty(4)
+# print(a4)
+
+#range array creating array from starting ti ending value 
+a5 =np.arange(2, 20, 2)
+# print(a5)
+
+#array diagonal element filled with 1's
+a6 = np.eye(3) # create 3by3 array dignoal with 1 
+# print(a6)
+# print()
+a7 = np.eye(5,5)
+# print(a7)
+
+#special type of aray where element come after a particular gap linespace
+a8 = np.linspace(0,20,num=5)
+print(a8)
